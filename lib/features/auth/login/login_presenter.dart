bff163af790b8d03258dbac60c5b27b98ceebdbb
essasabbagh <essasabbagh@gmail.com>
2023-12-28 00:52:06 +0300

    Enable login button

--- lib/features/auth/login/login_presenter.dart
+++ lib/features/auth/login/login_presenter.dart
@@ -1,15 +1,36 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_demo/core/utils/bloc_extensions.dart';
+import 'package:flutter_demo/core/utils/either_extensions.dart';
+import 'package:flutter_demo/features/auth/domain/use_cases/log_in_use_case.dart';
 import 'package:flutter_demo/features/auth/login/login_navigator.dart';
 import 'package:flutter_demo/features/auth/login/login_presentation_model.dart';
+import 'package:flutter_demo/localization/app_localizations_utils.dart';
 
 class LoginPresenter extends Cubit<LoginViewModel> {
   LoginPresenter(
     LoginPresentationModel super.model,
     this.navigator,
+    this.logInUseCase,
   );
 
   final LoginNavigator navigator;
+  final LogInUseCase logInUseCase;
 
   // ignore: unused_element
   LoginPresentationModel get _model => state as LoginPresentationModel;
+
+  Future<void> onLoginClicked() async {
+    await await logInUseCase
+        .execute(username: state.userName, password: state.userPassword) //
+        .observeStatusChanges(
+          (result) => emit(_model.copyWith(result)),
+        )
+        .asyncFold(
+          (fail) => navigator.showError(fail.displayableFailure()),
+          (success) => navigator.showAlert(
+            title: appLocalizations.loginSuccessTitle,
+            message: appLocalizations.loginSuccessMessage,
+          ),
+        );
+  }
 }
